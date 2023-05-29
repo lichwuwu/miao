35f39dea569b78823a89947250255e249cc2d059
lcw <lzj@qq.com>
2023-05-29 22:47:07 +0800

    1

--- oop.js
+++ oop.js
@@ -217,3 +217,37 @@ class MyMap {
     }
   }
 }
+class Complex {
+  constructor(real, imag){
+
+    this.real = real
+    this.imag = imag
+  }
+  plus(c) {
+    var real = this.real + c.real
+    var imag = this.imag + c.imag
+    return new Complex.constructor(real, imag)
+  }
+  minus(c) {
+    var real = this.real - c.real
+    var imag = this.imag - c.imag
+    return new Complex.constructor(real, imag)
+  }
+  mul(c) {
+    var real = this.real * c.real - this.imag * c.imag
+    var imag = this.real * c.imag + this.imag * c.real
+    return new Complex.constructor(real, imag)
+  }
+  div(c) {
+    var helper = new Complex.constructor(c.real, -c.imag)
+    var up = this.mul(helper)
+    var down = c.mul(helper) // down的虚部应该是0
+    var real = up.real / down.real
+    var imag = up.imag / down.real
+    return new Complex.constructor(real, imag)
+  }
+}
+
+
+
+
