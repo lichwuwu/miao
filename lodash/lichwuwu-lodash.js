d63efef30ac17c73e342579b7dc7879d5f903a65
lcw <lzj@qq.com>
2023-06-02 18:23:08 +0800

    1

--- lodash/lichwuwu-lodash.js
+++ lodash/lichwuwu-lodash.js
@@ -6,5 +6,14 @@ var lichwuwu = {
       result.push(array.slice(i,i+size))
     }
     return result
+  },
+  compact : function (array){
+    var result = []
+    for(var i = 0 ;i<array.length;i++){
+      if(array[i] != 0 && array[i] != undefined && isNaN(array[i]) == false && array[i] !== null){
+        result.push(array[i])
+      }
+    }
+    return result
   }
 }
